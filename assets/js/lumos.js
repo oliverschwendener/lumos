6efb72934db6b8a49626571b7e2dc20d313d1fec
oliverschwendener <oliver.schwendener@hotmail.com>
2016-01-23 00:30:23 +0100

    Version 1.0.0

--- assets/js/lumos.js
+++ assets/js/lumos.js
@@ -1,17 +1,19 @@
 // Inject HTML Frame
-$("body").append('<div class="lumos-container"><div class="img-alt-text"></div><img class="lumos-img" src="" /><svg version="1.1" class="lumos-prev" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 306 306" xml:space="preserve"><g><g id="chevron-right"><polygon points="211.7,306 247.4,270.3 130.1,153 247.4,35.7 211.7,0 58.7,153" /></g></g></svg><svg version="1.1" class="lumos-next" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 306 306" xml:space="preserve"><g><g id="chevron-right"><polygon points="94.35,0 58.65,35.7 175.95,153 58.65,270.3 94.35,306 247.35,153" /></g></g></svg><svg version="1.1" class="lumos-close" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve"><path d="M512,51.75L460.25,0L256,204.25L51.75,0L0,51.75L204.25,256L0,460.25L51.75,512L256,307.75L460.25,512L512,460.25 L307.75,256L512,51.75z" /></svg><div class="lumos-name"></div><div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div></div>');
+$("body").append('<div class="lumos-container"><div class="lumos-alt-text"></div><img class="lumos-img" src="" /><svg version="1.1" class="lumos-prev" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 306 306" xml:space="preserve"><g><g id="chevron-right"><polygon points="211.7,306 247.4,270.3 130.1,153 247.4,35.7 211.7,0 58.7,153" /></g></g></svg><svg version="1.1" class="lumos-next" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 306 306" xml:space="preserve"><g><g id="chevron-right"><polygon points="94.35,0 58.65,35.7 175.95,153 58.65,270.3 94.35,306 247.35,153" /></g></g></svg><svg version="1.1" class="lumos-close" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve"><path d="M512,51.75L460.25,0L256,204.25L51.75,0L0,51.75L204.25,256L0,460.25L51.75,512L256,307.75L460.25,512L512,460.25 L307.75,256L512,51.75z" /></svg><div class="lumos-name"></div><div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div></div>');
 
 // Global Variables
 var lumosContainer = $(".lumos-container");
-var lumosImage = $(".lumos-container .lumos-img");
+var lumosImage = $(".lumos-img");
+var altText = $(".lumos-alt-text");
 var prevButton = $(".lumos-prev");
 var nextButton = $(".lumos-next");
 var closeButton = $(".lumos-close");
 var linkSelector = ".lumos-link";
 var lumosGalleryName = $(".lumos-name");
-var allButtons = ".lumos-next, .lumos-prev, .lumos-close";
+var allButtons = ".lumos-next, .lumos-prev, .lumos-close, .lumos-alt-text";
 var spinner = $(".lumos-container .spinner");
 var animationSpeed = 250;
+var fadeInSpeed = 750;
 var mouseIsOnImage = false;
 var isSingleImage = false;
 var isFirst = false;
@@ -96,17 +98,18 @@ function openLumos(imageToOpen, galleryName) {
     else {
         checkButtons($('[class=lumos-link][href="' + imageToOpen + '"]'), galleryName);
     }
-    
+
     disableScroll();
     spinner.show();
     lumosContainer.fadeIn(animationSpeed);
     lumosImage.attr("src", imageToOpen);
 
     lumosContainer.imagesLoaded(function () {
-        lumosImage.fadeIn(animationSpeed);
+        lumosImage.fadeIn(fadeInSpeed);
     });
 
     lumosGalleryName.text(galleryName);
+    getAltText();
 }
 
 // Next
@@ -115,21 +118,24 @@ function getNextImage(direction) {
     lumosImage.hide();
 
     var currentGalleryName = lumosGalleryName.text();
+    var currentGallery = $("[data-lumos='" + currentGalleryName + "']");
     var currentImage = lumosImage.attr("src");
     var nextImage;
 
-    if (direction == "next") {
-        nextImage = $("[href='" + currentImage + "']").next(".lumos-link");
-    }
-    if (direction == "prev") {
-        nextImage = $("[href='" + currentImage + "']").prev(".lumos-link");
+    for (var i = 0; i < currentGallery.length; i++) {
+        if (currentGallery[i].getAttribute("href") == currentImage) {
+            if (direction == "next") { i++; }
+            if (direction == "prev") { i--; }
+            nextImage = currentGallery[i];
+            break;
+        }
     }
 
-    if (nextImage.attr("data-lumos") != currentGalleryName) {
+    if (nextImage.getAttribute("data-lumos") != currentGalleryName) {
         return currentImage;
     }
 
-    return nextImage.attr("href");
+    return nextImage.getAttribute("href");
 }
 
 // Close
@@ -138,6 +144,7 @@ function closeLumos() {
     lumosContainer.fadeOut(animationSpeed);
     lumosImage.attr("src", "");
     lumosGalleryName.text("");
+    altText.text("");
     spinner.hide();
     isSingleImage = false;
     isFirst = false;
@@ -184,6 +191,14 @@ function checkButtons(nextImage, galleryName) {
     }
 }
 
+// Get Alt Text
+function getAltText() {
+    var currentImage = lumosImage.attr("src");
+    var currentAltText = $("[href='" + currentImage + "']").children("img").attr("alt");
+    if (currentAltText != undefined) {
+        altText.html(currentAltText);
+    }
+}
 
 // Disable Scrolling
 var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
