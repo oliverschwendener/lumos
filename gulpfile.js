c8fb676b9dc10629836af70a9d842c9062beeea4
Oliver Schwendener <oliver.schwendener@frontify.com>
2019-07-24 09:40:27 +0200

    Updated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,62 @@
-'use strict';
-
 const gulp = require('gulp');
-const concat = require('gulp-concat');
 const minify = require('gulp-minify');
 const sass = require('gulp-sass');
-const minifyCss = require('gulp-minify-css');
 const browserSync = require('browser-sync');
 
-const options = {
-    port: 8080,
-    open: false,
-    reloadOnRestart: true,
-    logFileChanges: true,
-    server: {
-        baseDir: ['./']
-    },
-    notify: false,
-};
-
-const demoFiles = {
-    src: './demo/demo.scss',
-    out: './demo/demo.css'
-}
+const server = browserSync.create();
 
 const sourceFiles = {
     js: './assets/js/lumos.js',
-    scss: './assets/scss/**/*.scss',
-    html: './index.html'
+    scss: './assets/scss/lumos.scss',
 }
 
-const scriptFilesToConcat = [
-    './node_modules/jquery-touchswipe/jquery.touchSwipe.min.js',
-    './node_modules/imagesloaded/imagesloaded.pkgd.min.js',
-    './assets/js/lumos.js'
-];
-
-const destinationFolders = {
+const outputLocations = {
     js: './dist/js',
     css: './dist/css',
 }
 
-gulp.task('scripts', () => {
-    return gulp
-        .src(scriptFilesToConcat)
-        .pipe(concat('lumos.js'))
+const filesToWatch = [
+    sourceFiles.js,
+    sourceFiles.scss,
+    './index.html',
+    './demo/demo.scss',
+];
+
+function buildJs(callback) {
+    gulp
+        .src(sourceFiles.js)
         .pipe(minify())
-        .pipe(gulp.dest(destinationFolders.js));
-});
+        .pipe(gulp.dest(outputLocations.js));
 
-gulp.task('sass', () => {
-    return gulp
-        .src('./assets/scss/lumos.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(minifyCss())
-        .pipe(gulp.dest(destinationFolders.css))
-        .pipe(browserSync.stream());
-});
+    callback();
+}
 
-gulp.task('demo', () => {
-    return gulp
-        .src('./demo/demo.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./demo'));
-});
+function buildCss(callback) {
+    gulp
+        .src(sourceFiles.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(outputLocations.css));
 
-gulp.task('build', [
-    'scripts',
-    'sass'
-]);
+    callback();
+}
 
-gulp.task('serve', ['build'], () => {
-    browserSync(options);
-    gulp.watch(sourceFiles.js, ['scripts']).on('change', browserSync.reload);
-    gulp.watch(sourceFiles.scss, ['sass']).on('change', browserSync.reload);
-    gulp.watch(sourceFiles.html).on('change', browserSync.reload);
-    gulp.watch(demoFiles.src, ['demo']).on('change', browserSync.reload);
-});
+function watch() {
+    gulp.watch(filesToWatch, gulp.series(buildJs, buildCss, reload));
+}
+
+function reload(done) {
+    server.reload();
+    done();
+}
+
+function serve(done) {
+    server.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    done();
+}
+
+exports.dev = gulp.series(serve, watch);
+exports.build = gulp.parallel(buildJs, buildCss);
